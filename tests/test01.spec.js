7f7432a9f793b7b56b96fef797f768e88f475305
Antal Lohár <a123lohar@gmail.com>
2023-09-12 11:02:01 +0200

    xpath locator

--- tests/test01.spec.js
+++ tests/test01.spec.js
@@ -4,21 +4,25 @@ const fs = require('fs');
 test('Google search and screenshot', async ({page}) => {
 
   await page.goto('https://google.com')
-  await page.click('text="Az összes elfogadása"');
+  await page.waitForTimeout(1000);
+  await page.locator('xpath=//*[@id="L2AGLb"]/div').click()
+  await page.waitForTimeout(1000);
   
   await page.fill('[name="q"]', 'tesla');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(500);
   await page.press('[name="q"]', 'Enter')
+  await page.waitForTimeout(1000);
 
  await page.click('text=Wikipedia');
  await page.waitForTimeout(2000);
   
   const screenshotBuffer = await page.screenshot();
-  fs.writeFileSync('wikipedia.png', screenshotBuffer);
+  fs.writeFileSync('wikipedia.jpg', screenshotBuffer);
   
   await page.waitForTimeout(2000);
   
-  await page.close();
+  await page.close(); 
  
 });
+  
 
