e02b5ca85e34ffe0ebf9a91c48dae66d837ac43b
Antal Lohár <a123lohar@gmail.com>
2023-09-26 21:50:59 +0200

    New login test

---
+++ tests/test04.spec.js
@@ -0,0 +1,32 @@
+const {test, expect} = require('@playwright/test')
+const fs = require('fs');
+
+test('Login Test', async ({page}) => {
+
+  await page.goto('https://hu.wikipedia.org/wiki/Kezd%C5%91lap')
+  await page.waitForTimeout(1000);
+  await page.locator('xpath=//*[@id="pt-login"]').click()
+  await page.waitForTimeout(1000);
+  
+  await page.fill('[name="wpName"]', 'Antaltesztelo');
+  await page.waitForTimeout(500);
+
+  await page.fill('[name="wpPassword"]', 'Tesztelo01');
+  await page.waitForTimeout(500);
+
+  await page.locator('xpath=//*[@id="wpLoginAttempt"]').click()
+
+  const screenshotBuffer = await page.screenshot();
+  fs.writeFileSync('wikipedia_login.jpg', screenshotBuffer);
+
+  await page.getByRole('link', { name: 'Kijelentkezés' }).click()
+  await page.waitForTimeout(1000);
+
+
+  
+  await page.close(); 
+ 
+});
+  
+  
+  
